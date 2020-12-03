bfb72809804ee3f211100483c23da48adea8157d
rebecca mulugeta <becc.mulugeta@gmail.com>
2020-12-03 23:24:44 +0000

    MVC sepration of concern

---
+++ controllers/productController.js
@@ -0,0 +1,36 @@
+const Product = require("../models/productModel");
+
+// Gets all products
+// Route /api/products
+async function getProducts(req, res) {
+  try {
+    const products = await Product.findAll();
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Gets a single product
+// Route /api/product/:id
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Route not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  getProducts,
+  getProduct,
+};
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const http = require("http");
-const products = require("./data/products.json");
+const { getProduct } = require("./controllers/productController");
+const products = require("./data/products");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.write(JSON.stringify(products));
-  res.end();
+  if (req.url === "/api/products" && req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } else if (
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === "GET"
+  ) {
+    const id = req.url.split("/")[3];
+    getProduct(req, res, id);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not Found" }));
+  }
 });
 
 const PORT = process.env.PORT || 5000;
